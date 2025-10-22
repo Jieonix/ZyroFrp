e1d2b6a3f099a00bc24f2b4aac4bed14ed7be4b4
Jieonix <sj2040804972@gmail.com>
2025-10-22 23:33:58 +0800

    登录页面样式调整

--- WEB/FrontEnd/src/assets/api.js
+++ WEB/FrontEnd/src/assets/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 // 发行阶段API
-axios.defaults.baseURL = 'https://zyroo.cn/backend/'
+// axios.defaults.baseURL = 'https://zyroo.cn/backend/'
 
 // 测试环境API
-// axios.defaults.baseURL = 'http://localhost:8085'
+axios.defaults.baseURL = 'http://localhost:8085'
